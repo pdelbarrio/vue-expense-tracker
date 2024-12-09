d3a114b60a22242c8b6264993e437b0b04b307b9
Pablo del Barrio <pablodbb@gmail.com>
2024-12-09 19:11:27 +0100

    add transaction logic implemented

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <Header />
   <div class="container">
-    <Balance :total="total" />
-    <IncomeExpenses :income="income" :expenses="expenses" />
+    <Balance :total="+total" />
+    <IncomeExpenses :income="+income" :expenses="+expenses" />
     <TransactionList :transactions="transactions" />
-    <AddTransaction />
+    <AddTransaction @transactionSubmitted="handleTransactionSubmitted" />
   </div>
 </template>
 
@@ -14,8 +14,11 @@ import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
+import { useToast } from "vue-toastification";
 
 import { ref, computed } from "vue";
+
+const toast = useToast();
 
 const transactions = ref([
   { id: 1, text: "Flower", amount: -19.99 },
@@ -50,4 +53,20 @@ const expenses = computed(() => {
     }, 0)
     .toFixed(2);
 });
+
+//Add transactopm
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateUniqueId(),
+    text: transactionData.text,
+    amount: transactionData.amount,
+  });
+
+  toast.success("Transaction added");
+};
+
+//Generate unique ID
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 1000000);
+};
 </script>
